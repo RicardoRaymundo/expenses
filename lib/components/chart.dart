2b86bdbc520932314c64c1e1149d7f8c7616182b
RicardoRaymundo <ricardoraymundao@gmail.com>
2023-01-02 13:11:18 +0000

    FEAT: created chart component

---
+++ lib/components/chart.dart
@@ -0,0 +1,65 @@
+import 'package:expenses/components/chart_bar.dart';
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  const Chart(this.recentTransactions, {super.key});
+
+  final List<Transaction> recentTransactions;
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        bool sameDay = recentTransactions[i].date.day == weekDay.day;
+        bool sameMonth = recentTransactions[i].date.month == weekDay.month;
+        bool sameYear = recentTransactions[i].date.year == weekDay.year;
+
+        if (sameDay && sameMonth && sameYear) {
+          totalSum += recentTransactions[i].value;
+        }
+      }
+
+      return {
+        'day': DateFormat.E().format(weekDay)[0],
+        'value': totalSum,
+      };
+    }).reversed.toList();
+  }
+
+  double get _weekTotalValue {
+    return groupedTransactions.fold(0, (previousValue, element) {
+      return previousValue + (element['value'] as double);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    groupedTransactions;
+    return Card(
+        elevation: 6,
+        margin: const EdgeInsets.all(20),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: groupedTransactions.map((e) {
+              return Flexible(
+                fit: FlexFit.tight,
+                child: ChartBar(
+                  value: e['value'] as double,
+                  label: e['day'] as String,
+                  percentage: _weekTotalValue == 0
+                      ? 0
+                      : (e['value'] as double) / _weekTotalValue,
+                ),
+              );
+            }).toList(),
+          ),
+        ));
+  }
+}
