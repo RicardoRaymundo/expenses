06acf34a55ded06d6f8eb9773bb1187505bf4351
RicardoRaymundo <ricardoraymundao@gmail.com>
2023-01-02 13:13:04 +0000

    FEAT: updated main file with new components && widgets refactoring && responsive updates

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,15 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'dart:math';
+import 'dart:io';
 
+import 'package:expenses/components/chart.dart';
 import 'package:expenses/components/transaction_form.dart';
 import 'package:expenses/components/transaction_list.dart';
 import 'package:expenses/models/transaction.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(const ExpensesApp());
 
@@ -15,6 +19,7 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     final themeData = ThemeData();
 
     return MaterialApp(
@@ -24,6 +29,7 @@ class ExpensesApp extends StatelessWidget {
         colorScheme: themeData.colorScheme
             .copyWith(primary: Colors.purple, secondary: Colors.amber),
         textTheme: themeData.textTheme.copyWith(
+          button: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           headline6: TextStyle(
             fontFamily: 'OpenSans',
             fontSize: 18,
@@ -53,31 +59,81 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final List<Transaction> _transactions = [
     Transaction(
+      id: 't0',
+      title: 'Conta antiga',
+      value: 400.76,
+      date: DateTime.now().subtract(Duration(days: 3)),
+    ),
+    Transaction(
       id: 't1',
       title: 'Novo Tenis de corrida',
       value: 310.76,
-      date: DateTime.now(),
+      date: DateTime.now().subtract(Duration(days: 1)),
     ),
     Transaction(
       id: 't2',
       title: 'Roupinha para o Belo',
       value: 250.2,
-      date: DateTime.now(),
+      date: DateTime.now().subtract(Duration(days: 4)),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Cartão de crédito',
+      value: 150.2,
+      date: DateTime.now().subtract(Duration(days: 5)),
+    ),
+    Transaction(
+      id: 't4',
+      title: 'Jantar com mozao',
+      value: 50.2,
+      date: DateTime.now().subtract(Duration(days: 3)),
+    ),
+    Transaction(
+      id: 't5',
+      title: 'Nata com mozao',
+      value: 100.2,
+      date: DateTime.now().subtract(Duration(days: 6)),
+    ),
+    Transaction(
+      id: 't6',
+      title: 'Breja com mozao',
+      value: 40.2,
+      date: DateTime.now().subtract(Duration(days: 4)),
+    ),
+    Transaction(
+      id: 't7',
+      title: 'BK com mozao',
+      value: 70.2,
+      date: DateTime.now().subtract(Duration(days: 2)),
     ),
   ];
 
-  void _addTransaction(String title, double value) {
+  bool _showChart = false;
+
+  List<Transaction> get _recentTransactions {
+    return _transactions.where((element) {
+      return element.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
+    }).toList();
+  }
+
+  void _addTransaction(String title, double value, DateTime date) {
     final newTransaction = Transaction(
       id: Random().nextDouble().toString(),
       title: title,
       value: value,
-      date: DateTime.now(),
+      date: date,
     );
     setState(() {
       _transactions.add(newTransaction);
     });
 
     Navigator.of(context).pop();
+  }
+
+  void _deleteTransaction(String id) {
+    setState(() {
+      _transactions.removeWhere((element) => element.id == id);
+    });
   }
 
   void _openTransactionFormModal(BuildContext context) {
@@ -89,41 +145,98 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  Widget _getIconButton(IconData icon, Function() fn) {
+    return Platform.isIOS
+        ? GestureDetector(
+            onTap: fn,
+            child: Icon(icon),
+          )
+        : IconButton(
+            icon: Icon(icon),
+            onPressed: fn,
+          );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Expenses'),
-        actions: [
-          IconButton(
-            onPressed: () => _openTransactionFormModal(context),
-            icon: Icon(Icons.add),
-          )
-        ],
+    final mediaQuery = MediaQuery.of(context);
+
+    bool isLandScape = mediaQuery.orientation == Orientation.landscape;
+
+    final iconList = Platform.isIOS ? CupertinoIcons.refresh : Icons.list;
+    final iconChart =
+        Platform.isIOS ? CupertinoIcons.refresh : Icons.show_chart;
+
+    final actions = [
+      if (isLandScape)
+        _getIconButton(
+          _showChart ? iconList : iconChart,
+          () {
+            setState(() {
+              _showChart = !_showChart;
+            });
+          },
+        ),
+      SizedBox(
+        width: 20,
       ),
-      body: SingleChildScrollView(
+      _getIconButton(
+        Platform.isIOS ? CupertinoIcons.add : Icons.add,
+        () => _openTransactionFormModal(context),
+      ),
+    ];
+
+    final AppBar appBar = AppBar(
+      title: Text('Despesas Pessoais'),
+      actions: actions,
+    );
+
+    final availableHeight = mediaQuery.size.height -
+        appBar.preferredSize.height -
+        mediaQuery.padding.top;
+
+    final page = SafeArea(
+      child: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            SizedBox(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blueAccent,
-                elevation: 5,
-                child: Text('Grafico'),
-              ),
-            ),
-            TransactionList(
-              transactions: _transactions,
-            )
+            if (_showChart || !isLandScape)
+              SizedBox(
+                  height: availableHeight * (isLandScape ? 0.8 : 0.3),
+                  child: Chart(_recentTransactions)),
+            if (!_showChart || !isLandScape)
+              SizedBox(
+                height: availableHeight * (isLandScape ? 1 : 0.7),
+                child: TransactionList(
+                    transactions: _transactions, onRemove: _deleteTransaction),
+              )
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _openTransactionFormModal(context),
-        child: Icon(Icons.add),
-      ),
     );
+
+    return Platform.isIOS
+        ? CupertinoPageScaffold(
+            navigationBar: CupertinoNavigationBar(
+              middle: Text('Despesas Pessoais'),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: actions,
+              ),
+            ),
+            child: page,
+          )
+        : Scaffold(
+            appBar: appBar,
+            body: page,
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: Platform.isIOS
+                ? SizedBox()
+                : FloatingActionButton(
+                    onPressed: () => _openTransactionFormModal(context),
+                    child: Icon(Icons.add),
+                  ),
+          );
   }
 }
